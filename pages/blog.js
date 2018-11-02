0e83376067646e6edb689e08a0148120ff5ba5ed
Denis Sidorenko <dongor7@gmail.com>
2018-11-02 09:36:07 +0300

    Initial commit

---
+++ pages/blog.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Blog = () => {
+    return (
+        <div>
+            Hello
+            <Link href='/'>Back</Link>
+        </div>
+    )
+};
+
+export default Blog;
